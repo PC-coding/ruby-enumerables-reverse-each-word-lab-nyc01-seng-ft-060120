455261ae7a8548db427b20c4af1e2ca6ee532468
Partap Chauhan <partapchauhan8@gmail.com>
2020-05-25 00:20:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split
   string1 = ""
   array.each do |words|
-    string1 += "#{words.reverse}"
+    string1 += "#{words.reverse} "
   end
   return string1.chop
 end
